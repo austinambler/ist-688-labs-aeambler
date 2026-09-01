a73bf18fe41de0b7817d2269b906f878a0ea6470
austinambler <aeambler@syr.edu>
2026-09-01 15:57:51 +0000

    update

---
+++ Lab2.py
@@ -0,0 +1,69 @@
+import streamlit as st
+from openai import OpenAI, AuthenticationError
+
+# Show title and description.
+st.title("Lab 2 Document question answering")
+st.write(
+    "Upload a document below and ask a question about it – GPT will answer! "
+    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
+)
+
+# Ask user for their OpenAI API key via `st.text_input`.
+# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
+# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
+openai_api_key = st.text_input("OpenAI API Key", type="password")
+
+key_is_valid = False
+
+# check to make sure api key is valid
+if openai_api_key:
+    try:
+        client = OpenAI(api_key=openai_api_key)
+        # Lightweight call just to check if the key works
+        client.models.list()
+        st.success("API key is valid ✅")
+        key_is_valid = True
+    except AuthenticationError:
+        st.error("Invalid OpenAI API key ❌")
+    except Exception as e:
+        st.error(f"Something went wrong: {e}")
+
+if not openai_api_key or not key_is_valid:
+    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+else:
+
+    # Create an OpenAI client.
+    client = OpenAI(api_key=openai_api_key)
+
+    # Let the user upload a file via `st.file_uploader`.
+    uploaded_file = st.file_uploader(
+        "Upload a document (.txt or .md)", type=("txt", "md")
+    )
+
+    # Ask the user for a question via `st.text_area`.
+    question = st.text_area(
+        "Now ask a question about the document!",
+        placeholder="Can you give me a short summary?",
+        disabled=not uploaded_file,
+    )
+
+    if uploaded_file and question:
+
+        # Process the uploaded file and question.
+        document = uploaded_file.read().decode()
+        messages = [
+            {
+                "role": "user",
+                "content": f"Here's a document: {document} \n\n---\n\n {question}",
+            }
+        ]
+
+        # Generate an answer using the OpenAI API.
+        stream = client.chat.completions.create(
+            model="gpt-4.1",
+            messages=messages,
+            stream=True,
+        )
+
+        # Stream the response to the app using `st.write_stream`.
+        st.write_stream(stream)
